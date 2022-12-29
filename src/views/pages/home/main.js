ee5d25e3b2725713e0a4f8b2983e0c255ac5b29d
Alecsandr <79801722+SENIA918@users.noreply.github.com>
2022-12-29 16:32:37 +0300

    доделать слайдер,добавить кнопку

--- src/views/pages/home/main.js
+++ src/views/pages/home/main.js
@@ -1,1 +1,25 @@
-console.log('\n>> Home page');
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  slidesPerView: 3,
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 3000,
+},
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
