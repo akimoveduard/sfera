7513b09c2b7101cda3fb9386514909d0cf6ceab2
Ivan9812 <100289335+Ivan9812@users.noreply.github.com>
2022-12-29 15:46:58 +0400

    apps version number 1
    apps version number 1

--- src/views/pages/apps/main.js
+++ src/views/pages/apps/main.js
@@ -1,1 +1,23 @@
-console.log('\n>> Apps page');
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView:5,
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
