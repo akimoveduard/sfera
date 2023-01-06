8a921965c4ee5a1454d9a1cecc96a1ecc0b3ac59
git config --global user.email sergo_235_s@mail.ru <sergo_235_s@mail.ru>
2023-01-06 18:50:18 +0300

    Landing, not ready, needs change. HTML convented to pug, pug is ready, but needs convents css to scss and need convents classes to bem methodology

--- src/views/pages/index/main.js
+++ src/views/pages/index/main.js
@@ -1,1 +1,34 @@
-console.log('\n>> Landing page');
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+//Bar-menu
+let list_active = document.querySelector('.menu').querySelectorAll('a')
+
+list_active.forEach(element =>{
+    element.addEventListener('click',function (){
+        list_active.forEach(nav=>nav.classList.remove('active'))
+        this.classList.add('active');
+        menu.classList.remove('active')
+        menuBtn.classList.remove('active')
+    })
+})
+
+const body = document.body;
+const shadow = document.querySelector(".shadow-off");
+const btnOn = document.querySelector(".menu-btn");
+
+function ofScroll() {
+  body.classList.toggle("off_scroll");
+}
+function onShadow() {
+  shadow.classList.toggle("shadow-on");
+}
+
+btnOn.addEventListener("click", () => {
+  ofScroll();
+  onShadow();
+});
