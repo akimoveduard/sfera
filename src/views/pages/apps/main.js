e1f10385d699c06b352f6f06589bd593df93e840
Akimov Eduard <95487679+akimoveduard@users.noreply.github.com>
2023-01-18 12:07:03 +0300

    feat: верстать home, apps, team с адаптивом

--- src/views/pages/apps/main.js
+++ src/views/pages/apps/main.js
@@ -1,1 +1,32 @@
 console.log('\n>> Apps page');
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 10,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const PugPlugin = require('pug-plugin');
 
 module.exports = (env, argv) => {
+  
   const isProd = argv.mode === 'production';
 
   return {
